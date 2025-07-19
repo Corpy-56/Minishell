6524e0ad4fafbc927ccb49d75df4ba3797bbd2d6
Amandine Gouin <agouin@f6r12s16.clusters.42paris.fr>
2025-07-19 12:20:13 +0200

    jai repare mes petites erreur et continuer la fonction ft_type_token

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: agouin <agouin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 12:39:18 by agouin            #+#    #+#             */
-/*   Updated: 2025/07/18 17:45:14 by agouin           ###   ########.fr       */
+/*   Updated: 2025/07/19 12:13:41 by agouin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ int	ft_error(int i, char *str)
 	//write(2, "Error\n", 7);
 	write(2, str, (ft_strlen(str)));
 	write(2, "\n", 1);
+	//clear tout ca permettrait de faire un \n ??
 	//exit(EXIT_FAILURE);
-	return (0);
+	return (-1);
 }
 
 int	white_space(char *str, int i)
@@ -107,6 +108,24 @@ void	ft_initialization_commande(t_cmd *commande)
 //	return (joined);
 //}
 
+int	ft_glued_redirection(char *str)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = -1;
+	while((str[i] == '<' || str[i] == '>') && str[i])
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	j = ft_isalnum(str[i]);
+	if (j == 1)
+		return (1);
+	return (0);
+	
+}
+
 void	ft_type_token(t_cmd *commande, t_tokens *b_debut)// verifier que ca marche bien 
 {
 	t_tokens	*p_actuel;
@@ -124,66 +143,69 @@ void	ft_type_token(t_cmd *commande, t_tokens *b_debut)// verifier que ca marche 
 	p_actuel = b_debut;
 	while (p_actuel)
 	{
-		if (ft_strncmp(p_actuel->str, "<<", 2) == 0)// && commande->heredoc == NULL)
-		{
-			if (commande->heredoc == NULL)
-				commande->heredoc = ft_strdup(p_actuel->next->str);
-			else if (p_actuel->next->str != NULL && commande->heredoc != NULL)// problematique car prend les "  "
-			{
+		//voir le heredoc plus tard 
+		////if (ft_strncmp(p_actuel->str, "<<", 2) == 0)// && commande->heredoc == NULL)
+		////{
+		////	if (commande->heredoc == NULL)
+		////		commande->heredoc = ft_strdup(p_actuel->next->str);
+		////	else if (p_actuel->next->str != NULL && commande->heredoc != NULL)// problematique car prend les "  "
+		////	{
 				
-				ft_strjoin(commande->heredoc, p_actuel->next->str);
-			}
-		}
-		else if (commande->heredoc != NULL && ft_strncmp(p_actuel->str, "<<", 2) == 0)
-		{
-			//commande = ft_calloc(sizeof(t_cmd), 1);
-			//if (commande == NULL)
-			//	return ;
-			//commande->next = NULL;	
-			//if (a_debut == NULL)
-			//{
-			//	a_debut = commande;
-			//	fin = commande;
-			//}
-			//else
-			//{
-			//	fin->next = commande;
-			//	fin = commande;
-			//}
-			//if (p_actuel->next->str != NULL)
-			//	commande->heredoc = ft_strdup(p_actuel->next->str);
-		}
-		else if (ft_strncmp(p_actuel->str, ">>", 2) == 0) // remplace automatiquement celui que je veyx pas
+		////		ft_strjoin(commande->heredoc, p_actuel->next->str);
+		////	}
+		////}
+		////else if (commande->heredoc != NULL && ft_strncmp(p_actuel->str, "<<", 2) == 0)
+		////{
+		////	//commande = ft_calloc(sizeof(t_cmd), 1);
+		////	//if (commande == NULL)
+		////	//	return ;
+		////	//commande->next = NULL;	
+		////	//if (a_debut == NULL)
+		////	//{
+		////	//	a_debut = commande;
+		////	//	fin = commande;
+		////	//}
+		////	//else
+		////	//{
+		////	//	fin->next = commande;
+		////	//	fin = commande;
+		////	//}
+		////	//if (p_actuel->next->str != NULL)
+		////	//	commande->heredoc = ft_strdup(p_actuel->next->str);
+		////}
+		if (ft_strncmp(p_actuel->str, ">>", 2) == 0)
 		{
 			if (commande->fd_int_put != -2)
-				commande->fd_int_put = 0;
-			commande->fd_int_put = open(p_actuel->next->str, O_CREAT | O_WRONLY, 0777);
+				close(commande->fd_int_put);
+			if (ft_glued_redirection(p_actuel->str) == 1)// faut enlever les quotes si il y en a 
+				commande->fd_int_put = open(ft_strtrim(p_actuel->str, ">"), O_CREAT | O_WRONLY, 0777);
+			else if (p_actuel->next->str != NULL)
+				commande->fd_int_put = open(p_actuel->next->str, O_CREAT | O_WRONLY, 0777);
 			if (commande->fd_int_put == -1)
-				return ; //faire une vrai sortie
-			p_actuel = p_actuel->next;
+				ft_error(1, "Permission denied") ;
 			printf("%d", commande->fd_int_put);
 		}
-		else if (ft_strncmp(p_actuel->str, "<", 1) == 0) // remplace automatiquement celui que je veyx pas
+		else if (ft_strncmp(p_actuel->str, "<", 1) == 0)// remplace automatiquement celui que je veyx pas
 		{
-			if (commande->fd_int_put != -2)
-				commande->fd_int_put = 0;// faire une condition si pas de str
-			commande->fd_int_put = open(p_actuel->next->str, O_CREAT | O_WRONLY, 0777);
+			if (commande->fd_out_put != -2)
+				close(commande->fd_int_put);
+			if (ft_glued_redirection(p_actuel->str) == 1)// faut enlever les quotes si il y en a 
+				commande->fd_int_put = open(ft_strtrim(p_actuel->str, "<"), O_CREAT | O_WRONLY, 0777);
+			else if (p_actuel->next->str != NULL)
+				commande->fd_int_put = open(p_actuel->next->str, O_CREAT | O_WRONLY, 0777);
 			if (commande->fd_int_put == -1)
-			{
-				printf("%s: Permission denied\n", p_actuel->next->str); //si besoin je peux faire un char * pour le nom du fichier
-				return ; //faire une vrai sortie
-			}
-			p_actuel = p_actuel->next;
+				ft_error(1, "Permission denied") ;
+			printf("%d", commande->fd_int_put);
 		}
-		//else if (ft_strncmp(p_actuel->str, ">>", 2) == 0)
-		//	commande->out_put = p_actuel->next->str;
-		//else if (ft_strncmp(p_actuel->str, ">", 1) == 0)// je sais pas si je dois faire la meme chose quavec les heredocs
-		//	commande->out_put = p_actuel->next->str;
-		else if (ft_test_commande(p_actuel->str) == 0 && commande->cmd == NULL)// marchera si un arg mais si plusieurs
-		{
-			commande->cmd = ft_strdup(p_actuel->str);
-			//commande->args = ft_strjoin_tab(commande->args, commande->cmd);
-		}
+		////else if (ft_strncmp(p_actuel->str, ">>", 2) == 0)
+		////	commande->out_put = p_actuel->next->str;
+		////else if (ft_strncmp(p_actuel->str, ">", 1) == 0)// je sais pas si je dois faire la meme chose quavec les heredocs
+		////	commande->out_put = p_actuel->next->str;
+		//else if (ft_test_commande(p_actuel->str) == 0 && commande->cmd == NULL)// marchera si un arg mais si plusieurs
+		//{
+		//	commande->cmd = ft_strdup(p_actuel->str);
+		//	//commande->args = ft_strjoin_tab(commande->args, commande->cmd);
+		//}
 		//else if (ft_strncmp(p_actuel->str, "|", 1) == 0)
 		//{
 		//	commande = ft_calloc(sizeof(t_cmd), 1);
@@ -229,69 +251,53 @@ int	ft_next_redirection(char *str)
 	return (0);
 }
 
-void	ft_valid_syntax(t_tokens *token)
+int	ft_valid_syntax(t_tokens *token)//gerer aussi les | et ;
 {
 	t_tokens	*p_actuel;
 
 	p_actuel = token;
-	if (ft_strncmp(p_actuel->str, "|", 2) == 0)
-		ft_error(1, "syntax error near unexpected token `|'");
+	if (ft_strncmp(p_actuel->str, "|", 1) == 0)
+		return (ft_error(1, "syntax error near unexpected token `|'"));
 	while(p_actuel != NULL)
 	{
 		if (p_actuel->next == NULL && ft_strncmp(p_actuel->str, "|", 1) == 0)
-			ft_error(1, "syntax error near unexpected token `|'");
+			return (ft_error(1, "syntax error near unexpected token `|'"));
 		else if (ft_strncmp(p_actuel->str, ">>>", 4) == 0 || ft_strncmp(p_actuel->str, "<<<", 4) == 0)
-			ft_error(1, "syntax error near unexpected token");
+			return (ft_error(1, "syntax error near unexpected token"));
 		else if (ft_strncmp(p_actuel->str, ">>>>", 4) == 0 || ft_strncmp(p_actuel->str, "<<<<", 4) == 0)
-			ft_error(1, "syntax error near unexpected token");
+			return (ft_error(1, "syntax error near unexpected token"));
 		else if (ft_strncmp(p_actuel->str, ">>", 2) == 0 || ft_strncmp(p_actuel->str, "<<", 2) == 0)
 		{
 			if (p_actuel->next == NULL && ft_next_redirection(p_actuel->str) == 1)
-				ft_error(1, "syntax error near unexpected token");
+				return (ft_error(1, "syntax error near unexpected token"));
 		}
 		else if (ft_strncmp(p_actuel->str, ">", 1) == 0 || ft_strncmp(p_actuel->str, "<", 1) == 0)
 		{
 			if (p_actuel->next == NULL && ft_next_redirection(p_actuel->str) == 1)
-				ft_error(1, "syntax error near unexpected token");
+				return (ft_error(1, "syntax error near unexpected token"));
 		}
 		p_actuel = p_actuel->next;
 	}
+	return (0);
 }
 
-int main(int argc, char **argv, char **env)
+int main(void)
 {
 	char *rl;
-	char *pwd;
 	t_shell	*stru;
 
-	(void)argc;
-	(void)argv;
 	stru = ft_calloc(sizeof(t_shell), 1);
 	if (stru == NULL)
 		return (0);
-	stru->environ = ft_duplicate_env(env);
 	while(1)
 	{
 		rl = readline("Minishell : ");
 		if(!rl)
-			exit(0);//besoin dune fonction pour free
+			exit(0);//besoin dune fonction pour free 
 		add_history(rl);
 		stru->tokens = ft_tokenisation(rl, stru->tokens);
-		ft_valid_syntax(stru->tokens);
-		ft_type_token(stru->commande, stru->tokens);
-		if(ft_strncmp(rl, "pwd", 4) == 0)
-		{
-			pwd = getcwd(NULL, 0);
-			printf("%s\n", pwd);
-		}
-		if (ft_strncmp(rl, "env", 4) == 0)
-		{
-			while(stru->environ != NULL)
-			{
-				printf("%s\n", stru->environ->str);
-				stru->environ = stru->environ->next;
-			}
-		}
+		if (ft_valid_syntax(stru->tokens) == 0)
+			ft_type_token(stru->commande, stru->tokens);
 	}
 	rl_clear_history ();
     return (0);
